389628f42316e67061a7ee81bedfc965e1c30cad
Majda FANNAN <fannanmajda123@gmail.com>
2021-05-28 00:02:04 +0100

    Ajoutez des fichiers projet.

---
+++ xamarincsharp/xamarincsharp/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace xamarincsharp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+           
+        }
+
+        //Methode de cycle de vie 
+
+        protected override void OnStart()
+        {
+            
+            Debug.WriteLine("Onstart");
+            if (Current.Properties.ContainsKey("MainPageID"))
+            {
+                var id = Current.Properties["MainPageID"];
+                Debug.WriteLine("Onstart-" + id);
+
+            }
+          
+
+        }
+
+        protected override void OnSleep()
+        {
+            Debug.WriteLine("OnSleep");
+        }
+
+        protected override void OnResume()
+        {
+            Debug.WriteLine("OnResume");
+            if (Current.Properties.ContainsKey("MainPageID"))
+            {
+                var id = Current.Properties["MainPageID"];
+                Debug.WriteLine("OnResume-" + id);
+
+            }
+        }
+    }
+}
